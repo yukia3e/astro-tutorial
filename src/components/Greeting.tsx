f061c679a50b521083b989ee5c513866027cf238
yukia3e <12268087+yukia3@users.noreply.github.com>
2023-02-10 15:10:41 +0900

    Add tutorial files

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,20 @@
+import { h } from 'preact';
+import type { FunctionComponent } from 'preact';
+import { useState } from 'preact/hooks';
+
+const Greeting: FunctionComponent<{messages: string[]}> = ({messages}) => {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+  
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div> 
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  );
+}
+export default Greeting;
